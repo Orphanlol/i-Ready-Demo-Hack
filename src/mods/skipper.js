bd18d898fc88466c6069110cb0a27123c72dfec2
wang <114349746+wang-on-top@users.noreply.github.com>
2023-06-30 15:54:03 -0400

    fix

--- src/mods/skipper.js
+++ src/mods/skipper.js
@@ -2,7 +2,7 @@ function skip() {
   if (document.getElementById('nav-forward') || html5Iframe.contentwindow.document.getElementById('nav-forward')) { //check if lesson is open by finding elements inside the lesson
     try {
         Object.values(document.getElementById('nav-forward'))[1].onClick() // this function wont work in regular i-ready but just in case (call the function that gets called when you go forward in the lesson)
-      } catch () {
+      } catch {
         Object.values(html5Iframe.contentwindow.document.getElementById('nav-forward'))[1].onClick() //call the function that gets called when you go forward in the lesson
     }
   } else {
